5b986144747c20af16c7f4c6fcae87597de5ac3d
HwangJenn <hwangjihyunh@naver.com>
2021-01-05 15:57:09 +0900

    Simple Operator

---
+++ SimpleOperator/AplusB.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class AplusB {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        scan.close();
+        System.out.println(a+b);
+    }
+}
