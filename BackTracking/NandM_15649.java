25db21537f3553ca7b86b73d0f7e271b34d46e85
JiHyun Hwang <76980015+HwangJenn@users.noreply.github.com>
2021-06-08 15:00:48 +0900

    백준_백트레킹

--- BackTracking/NandM_15649.java
+++ BackTracking/NandM_15649.java
@@ -1,17 +1,21 @@
 //자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
 //1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
 import java.util.*;
+
 public class NandM_15649 {
     public static int[] arr;
     public static boolean[] visit;
+    
     public static void main(String[] args){
+        
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
         int m = scan.nextInt();
         arr = new int[m];
         visit = new boolean[n];
         dfs(n, m, 0);
-    }   public static void dfs(int n, int m, int depth){
+    }   
+    public static void dfs(int n, int m, int depth){
         if(depth==m){
             for(int val : arr){
                 System.out.print(val + " ");
