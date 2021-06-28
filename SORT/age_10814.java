db3ae9a19f24288a923d2770411ef5049233a951
JiHyun Hwang <76980015+HwangJenn@users.noreply.github.com>
2021-06-28 13:15:45 +0900

    백준_정렬

--- SORT/age_10814.java
+++ SORT/age_10814.java
@@ -2,20 +2,24 @@
 import java.util.Scanner;
 import java.util.Arrays;
 import java.util.Comparator;
+
 public class age_10814 {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
         String[][] arr = new String[n][2];
+        
         for(int i=0; i<n; i++){
             arr[i][0]= scan.next();
             arr[i][1]= scan.next();
-        }Arrays.sort(arr, new Comparator<String[]>() {
+        }
+        Arrays.sort(arr, new Comparator<String[]>() {
             @Override
             public int compare(String[] o1, String[] o2) {
                 return Integer.parseInt(o1[0])-Integer.parseInt(o2[0]);
             }
-        }); for(int i=0; i<n; i++){
+        }); 
+        for(int i=0; i<n; i++){
             System.out.println(arr[i][0]+ " "+ arr[i][1]);
         }
     }
